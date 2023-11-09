5fc8052e8d4da4250607740ba934123b69e6c0c1
Carlos Godoy <carlosgodoy@pursuit.org>
2023-11-08 21:47:18 -0500

    adding JS Docs

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Reverses the order of words in a string.
+ *
+ * @param {string} inputString - The input string with words separated by spaces.
+ * @returns {string} The input string with words in reverse order.
+ */
+ function reverseWords(inputString) {
+    // Implementation goes here
+  }
+  
